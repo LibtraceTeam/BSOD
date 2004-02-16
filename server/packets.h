8e7e5671e7b1c1cc735e94aed6b3b311bf2e9946
Brendon Jones <brendonj@waikato.ac.nz>
2004-02-16 01:14:58 +0000

    Changed to work with the new (almost) c++ code.

--- server/packets.h
+++ server/packets.h
@@ -5,4 +5,4 @@ extern int get_start_pos(float start[3], struct in_addr source, int iface);
 extern int get_end_pos(float end[3], struct in_addr dest, int iface);
 extern int per_packet(const dag_record_t *erfptr, uint32_t caplen, uint64_t ts/*, int new_fd*/);
 extern void empty_flows();
-extern void init_flow_hash(int fd);
+extern void init_packets(int new_fd);
--- server/socket.h
+++ server/socket.h
@@ -2,5 +2,5 @@ extern int setup_listen_socket();
 extern int bind_tcp_socket(int listener, int port);
 extern int check_client(int *fdmax, fd_set *master);
 extern int send_new_flow(int new_fd, float start[3], float end[3], uint32_t count);
-extern int send_new_packet(int new_fd, uint64_t ts, uint32_t id, char colour[3], uint16_t size);
+extern int send_new_packet(int new_fd, uint64_t ts, uint32_t id, uint8_t colour[3], uint16_t size);
 extern int send_kill_flow(int new_fd, uint32_t id);

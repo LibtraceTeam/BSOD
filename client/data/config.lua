215bef74bc4df433ff83ca47739c909eabdb19b9
Brendon Jones <brendonj@waikato.ac.nz>
2005-01-27 21:01:42 +0000

    Added some documentation to the config file.
    Removed all the leftover pictures from the data/ dir that we no longer need

--- client/data/config.lua
+++ client/data/config.lua
@@ -1,17 +1,21 @@
---start_location	= {-3.50,1.53,24.0}
+-- see the README.txt for more information about these configuration options.
+-- You WILL need to change the network_host option in order for this to work.
+
+-- start location for the camera {X, Y, Z} coordinates
 start_location	= {-0.941564,2.347431,31.202248}
+-- pitch (angle upwards/downwards) the camera should start with
 pitch = 11.199999
+-- heading (angle left/right) the camera should start with
 heading = 4.499999
 
-display			=  "opengl" -- "d3d" -- 
+display			=  "opengl"
 fullscreen		= "yes"
+
+-- resolution the bsod client should run in
 width			= 1024
 height			= 768
 bpp			= 32
 
--- Hawk net driver can use "host:port" notation
-network_host	= "voodoo:12345"
---network_host    = "chasm:32500"
---network_host  = "voodoo:32500"
--- Do not specify port for enet (for now)
---network_host    = "localhost" 
+-- host running the bsod server, and the port the server is listening on
+-- in "host:port" notation
+network_host    = "host:32500"

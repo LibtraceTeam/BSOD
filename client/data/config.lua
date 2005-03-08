55a154c5e45dd19f8481ef72ce4bce22af724269
Brendon Jones <brendonj@waikato.ac.nz>
2005-03-08 03:48:45 +0000

    Added images for the matrix mode.
    Is this something we actually want? Copyright issues?

--- client/data/config.lua
+++ client/data/config.lua
@@ -1,21 +1,30 @@
--- see the README.txt for more information about these configuration options.
--- You WILL need to change the network_host option in order for this to work.
+--start_location	= {-3.50,1.53,24.0}
+--start_location	= {-0.941564,2.347431,31.202248}
+start_location	= {4.23,0.00,21.73}
+--pitch = 11.199999
+--heading = 4.499999
+pitch = 1.0
+heading = 12.3
 
--- start location for the camera {X, Y, Z} coordinates
-start_location	= {-0.941564,2.347431,31.202248}
--- pitch (angle upwards/downwards) the camera should start with
-pitch = 11.199999
--- heading (angle left/right) the camera should start with
-heading = 4.499999
-
-display			=  "opengl"
-fullscreen		= "yes"
-
--- resolution the bsod client should run in
+display			=  "opengl" -- "d3d" -- 
+fullscreen		= "no"
 width			= 1024
 height			= 768
 bpp			= 32
 
--- host running the bsod server, and the port the server is listening on
--- in "host:port" notation
-network_host    = "host:32500"
+speed = 1.0
+size = 2.0
+jitter = "yes"
+billboard = "no"
+particle = "data/particle.png"
+do_gcc = "yes"
+matrix_mode = "no"
+
+-- Hawk net driver can use "host:port" notation
+network_host	= "voodoo:12345"
+--network_host	= "voodoo.cs.waikato.ac.nz:2345"
+--network_host    = "chasm:32500"
+--network_host  = "voodoo:32500"
+--network_host	= "voodoo:2345" --Slammer
+-- Do not specify port for enet (for now)
+--network_host    = "localhost" 

eade34a058b0c56885725858f58ed45b33a24725
Perry Lorier <perry@waikato.ac.nz>
2008-05-08 08:59:13 +0000

    Don't delete the current iterator

--- server/RTTMap.cpp
+++ server/RTTMap.cpp
@@ -82,15 +82,22 @@ void RTTMap::Add( Flow *flow, unsigned long int time_stamp, double now )
 int RTTMap::Flush( double now )
 {
 	FlowMap::iterator i = m_flows->begin();
+	FlowMap::iterator inew;
 	int count = 0;
 
-	for( ; i != m_flows->end(); i++ )
+	for( ; i != m_flows->end(); i=inew )
 	{
+		inew=i;
+		++inew;
+
 		TraceMap &tmap = i->second;
 		TraceMap::iterator j = tmap.begin();
+		TraceMap::iterator jnew;
 
-		for( ; j != tmap.end(); j++ )
+		for( ; j != tmap.end(); j=jnew )
 		{
+			jnew=j;
+			++jnew;
 			if( (now - j->second) > 180 )
 			{
 				tmap.erase( j );

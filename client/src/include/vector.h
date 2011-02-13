10c0d6a6b968d1293a5d4dba0ed6c5019f01d711
Shane Alcock <salcock@waikato.ac.nz>
2011-02-13 22:02:09 +0000

    * Added licensing to all source files  * Enabled Id keyword for all source files

--- client/src/include/vector.h
+++ client/src/include/vector.h
@@ -1,3 +1,34 @@
+/* 
+ * This file is part of BSOD client
+ *
+ * Copyright (c) 2011 The University of Waikato, Hamilton, New Zealand.
+ *
+ * Author: Paul Hunkin
+ *
+ * Contributors: Shane Alcock
+ *
+ * All rights reserved.
+ *
+ * This code has been developed by the University of Waikato WAND research
+ * group. For further information please see http://www.wand.net.nz/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+
+
 /*******************************************************************************
 							BSOD2 Client - vector.h
 							
